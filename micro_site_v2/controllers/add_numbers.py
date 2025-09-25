d0cec6230c13b08c2e0ffdc7467999f91d8079af
djangouser <kmm2459356@stu.o-hara.ac.jp>
2025-09-25 11:23:48 +0900

    aa

---
+++ micro_site_v2/controllers/add_numbers.py
@@ -0,0 +1,11 @@
+from utils import render_template
+
+
+def add_numbers(environ):
+    # ここに処理を書く
+    # set_first_value()
+    # set_second_value()
+    # get_addition()
+
+    # additionの結果を渡す
+    return render_template("boundaries/add_numbers_data.html")
